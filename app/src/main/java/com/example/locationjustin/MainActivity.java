9c42e3cb21dcbffff65384e9883384d303e161b8
SamilaChanu95 <chanukasamila@gmail.com>
2019-09-07 01:16:48 +0530

    Update the  google api services.

--- app/src/main/java/com/example/locationjustin/MainActivity.java
+++ app/src/main/java/com/example/locationjustin/MainActivity.java
@@ -2,15 +2,49 @@ package com.example.locationjustin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+//using the google api service must implement this interfaces
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
 
     public static final String TAG = "TAG";
+    private static final int REQUEST_CODE = 1000;
+
+    private GoogleApiClient googleApiClient;
+    private Location location;
+    private TextView txtLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtLocation = (TextView) findViewById(R.id.txtLocation);
+
+        googleApiClient = new GoogleApiClient.Builder(MainActivity.this)
+                .addConnectionCallbacks(MainActivity.this)
+                .addOnConnectionFailedListener(MainActivity.this)
+                .addApi(LocationServices.API).build();
+     }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
